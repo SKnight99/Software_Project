ad09b60ddcb24ecebd96d569bdbb6d87f230206e
Muhammad Fardan Rashidi <48272291+SKnight99@users.noreply.github.com>
2021-11-08 11:45:45 +0800

    Update login.php

--- php/login.php
+++ php/login.php
@@ -3,18 +3,16 @@
     $password = $_POST['password'];
 
     $conn = new mysqli('localhost','root','','people_health_pharmacy');
-    $query="SELECT * From employees WHERE username = $username";
+    $query="SELECT * From employees WHERE username = '$username' AND pwd = '$password'";
     $query_run=mysqli_query($conn,$query);
     $usertypes = mysqli_fetch_array($query_run);
 
-    if($password == $usertypes['pwd'])
-    {
-      if ($usertypes['position'] == "Employer") {
-        header("Location: ../employee.php");
-      }else if ($usertypes['position'] == "Employee"){
-        header("Location: ../product/displayproduct.php");
-      }
+    if ($usertypes['position'] == "Employer") {
+      header("Location: ../employee.php");
+    }else if ($usertypes['position'] == "Employee"){
+      header("Location: ../product/displayproduct.php");
+    }else {
+      header("Location: ../index.html");
     }
 
-    header("Location: ../employee.php");
 ?>
