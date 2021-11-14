a196fc8938d5db33cda11f3966837e5e5149698c
Muhammad Fardan Rashidi <48272291+SKnight99@users.noreply.github.com>
2021-11-14 23:48:19 +0800

    Add files via upload

---
+++ php/Generate.php
@@ -0,0 +1,20 @@
+<?php
+
+// output headers so that the file is downloaded rather than displayed
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=data.csv');
+
+// create a file pointer connected to the output stream
+$output = fopen('php://output', 'w');
+
+// output the column headings
+fputcsv($output, array('Column 1', 'Column 2', 'Column 3'));
+
+// fetch the data
+$conn = new mysqli('localhost','root','','people_health_pharmacy');
+$rows = mysqli_query($conn, 'SELECT * FROM product'); //WHERE MONTH(happened_at) = $month AND YEAR(happened_at) = $year
+
+// loop over the rows, outputting them
+while ($row = mysqli_fetch_assoc($rows)) fputcsv($output, $row);
+
+?>
