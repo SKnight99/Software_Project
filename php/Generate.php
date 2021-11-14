3cf8f549527f7c69c5ed6c29aedbdca167e228aa
Muhammad Fardan Rashidi <48272291+SKnight99@users.noreply.github.com>
2021-11-15 00:11:06 +0800

    Update Generate.php

--- php/Generate.php
+++ php/Generate.php
@@ -10,9 +10,11 @@ $output = fopen('php://output', 'w');
 // output the column headings
 fputcsv($output, array('Column 1', 'Column 2', 'Column 3'));
 
+$month = $_POST['month'];
+$year = $_POST['year'];
 // fetch the data
 $conn = new mysqli('localhost','root','','people_health_pharmacy');
-$rows = mysqli_query($conn, 'SELECT * FROM product'); //WHERE MONTH(happened_at) = $month AND YEAR(happened_at) = $year
+$rows = mysqli_query($conn, "SELECT * FROM product WHERE MONTH(happened_at) = $month AND YEAR(happened_at) = $year"); 
 
 // loop over the rows, outputting them
 while ($row = mysqli_fetch_assoc($rows)) fputcsv($output, $row);
