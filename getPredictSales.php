0fe88bd1afd6d046ab8a250d25d0b1b8c79f3341
Poovendran08 <93114368+Poovendran08@users.noreply.github.com>
2021-11-22 19:25:56 +0800

    Add files via upload
    predict sales of item,  done

---
+++ getPredictSales.php
@@ -0,0 +1,55 @@
+<?php
+$pageTitle = "Predict Highest Sales Of Month";
+$additionalReferences = '
+<link rel="stylesheet" href="css/modal.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+';
+include "header.php";
+?>
+
+<div id="content__header">
+    <span class="title">
+        Predict Highest Sales Of Month
+    </span>
+</div>
+<div id="content">
+    
+  
+   <?php
+    $month = $_POST['month'];
+    $conn=new mysqli('localhost','root','','people_health_pharmacy');
+    
+    $result = mysqli_query($conn ,"SELECT ProductName,EXTRACT(MONTH FROM InvoiceDate) AS InvoiceDate, MAX(Quantity)AS Quantity FROM sales");
+  ?>
+  <table class="table table-bordered" id="dashboard" width="100%" cellspacing="0">
+    <thead>
+      <tr>
+        <th scope="col">Product Name</th>
+        <th scope="col">Month</th>
+          <th scope="col">Highest sales</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      while($row = mysqli_fetch_array($result))
+          {
+              echo "<tr>";
+                 echo "<td>" . $row['ProductName'] . "</td>";
+                 echo "<td>" . $row['InvoiceDate'] . "</td>";
+                  echo "<td>" . $row['Quantity'] . "</td>";
+                  //echo "<td>" . $row['SalesID'] . "</td>";
+                  //echo "<td>" . $row['ProductName'] . "</td>";
+                  //echo "<td>" . $row['InvoiceDate'] . "</td>";
+                  //echo "<td>" . $row['InvoiceNo'] . "</td>";
+                  //echo "<td>" . $row['Quantity'] . "</td>";
+                  //echo "<td>" . $row['TotalSales'] . "</td>";
+              echo "</tr>";
+          }
+    ?>
+    </tbody>
+  </table>   
+    
+
+</div>
+
+<?php include "footer.php"; ?>
---
+++ predictSales.php
@@ -0,0 +1,25 @@
+<?php
+$pageTitle = "Sale Reports";
+$additionalReferences = '
+<link rel="stylesheet" href="css/modal.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+';
+include "header.php";
+?>
+
+<div id="content__header">
+    <span class="title">
+        Sales Report
+    </span>
+</div>
+<div id="content">
+  <form action="getPredictSales.php" method="post">
+    <br>
+      &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+      <input type="text"  name="month" placeholder="Month (1-12)" required><br>
+      <input type="submit" name = "generate " value="Generate">
+  </form>
+</div>
+
+
+<?php include "footer.php"; ?>
